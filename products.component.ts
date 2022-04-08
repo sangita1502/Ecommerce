6715ed90f34c2a824eb33c1cbf55db3a8174d063
sangita1502 <98583810+sangita1502@users.noreply.github.com>
2022-04-08 13:42:06 +0530

    Add files via upload

--- products.component.ts
+++ products.component.ts
@@ -1,23 +1,37 @@
+import { CookieServiceService } from './../../services/cookie-service.service';
 import { Component, OnInit } from '@angular/core';
-import { ApiServiceService } from '../services/api-service.service';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
-  selector: 'app-products',
+  selector: 'app-admin-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  baseurl = this.api.baseurl;
+  products: any;
+  id: string = "";
 
-  products:any = null;
-
-  constructor(public api:ApiServiceService) { 
-    
-  }
+  constructor(private api: ApiServiceService, private cookie: CookieServiceService) { }
 
   ngOnInit(): void {
-    this.api.post("product/list", {}).subscribe((data:any)=>{
-      this.products = data.data;
+    this.list();
+  }
+  list() {
+    this.api.post("product/list", { data: {} }).subscribe((mydata: any) => {
+      this.products = mydata.data;
+      console.log(this.products);
+
     });
+  }
+
+  delete(id: string): void {
+    if (confirm("Sure to delete?")) {
+      let data = { id: id };
+      this.api.post("product/delete", { data: data }).subscribe((mydata: any) => {
+        this.list();
+      });
+    }
   }
 
 }
